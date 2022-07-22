b26bdc2aa9d7bc778ff777d0e67f8f08d7fc1a4c
Kyeyune Ben <benkyeyune10@gmail.com>
2022-07-22 08:15:54 +0300

    v

--- library/settings.py
+++ library/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'books.apps.BooksConfig'
+    'books.apps.BooksConfig',
+    'bootstrap5'
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
+# Heroku settings.
+import django_heroku
+django_heroku.settings(locals())
